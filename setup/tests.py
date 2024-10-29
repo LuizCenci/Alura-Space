ec8d5756dbca29957ce3a06e9b31796ade23f752
LuizCenci <155825996+LuizCenci@users.noreply.github.com>
2024-10-29 10:47:15 -0300

    new storage system not working

--- setup/tests.py
+++ setup/tests.py
@@ -1,8 +1,10 @@
-from django.test import TestCase
 from pathlib import Path
 import os
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'setup/static')
 ]
-print(STATICFILES_DIRS)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+print(STATIC_ROOT)
